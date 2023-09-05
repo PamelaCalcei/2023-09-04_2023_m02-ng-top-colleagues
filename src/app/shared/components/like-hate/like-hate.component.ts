41dafd9f7ae41717f3adade4004256a51adb0753
unknown <pecalcei@gmail.com>
2023-09-05 14:53:14 +0200

    ajout bouton j'aime et je déteste

---
+++ src/app/shared/components/like-hate/like-hate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-like-hate',
+  templateUrl: './like-hate.component.html',
+  styleUrls: ['./like-hate.component.scss']
+
+})
+export class LikeHateComponent {
+
+
+}
